35ac2a48b129f7186ea81f991da6ccc1e1b925b5
CAIO AGUIAR <caio.athanasio.de.sousa.aguiar@tecnico.ulisboa.pt>
2025-11-05 17:41:16 +0000

    Refactoring the types of transaction status and transaction payment methods

--- data-manipulation-in-ts/src/types/transaction.types.ts
+++ data-manipulation-in-ts/src/types/transaction.types.ts
@@ -1,16 +1,21 @@
+export const TRANSACTION_PAY_METHODS = ["CREDIT_CARD", "BANK_SLIP"] as const;
+
+export const TRANSACTION_STATUSES = [
+  "PAID",
+  "DECLINED_BY_THE_CARD_ISSUER",
+  "AWAITING_PAYMENT",
+  "REVERSED",
+] as const;
+
 /**
  * The possible payment methods for a transaction
  */
-export type TransactionPaymentMethod = "CREDIT_CARD" | "BANK_SLIP";
+export type TransactionPaymentMethod = (typeof TRANSACTION_PAY_METHODS)[number];
 
 /**
  *
  */
-export type TransactionStatus =
-  | "PAID"
-  | "DECLINED_BY_THE_CARD_ISSUER"
-  | "AWAITING_PAYMENT"
-  | "REVERSED";
+export type TransactionStatus = (typeof TRANSACTION_STATUSES)[number];
 
 /**
  *
