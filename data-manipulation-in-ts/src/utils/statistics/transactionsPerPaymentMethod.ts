377559dd24c265bf1e854d11196fe61b4e88f111
CAIO AGUIAR <caio.athanasio.de.sousa.aguiar@tecnico.ulisboa.pt>
2025-11-05 18:11:05 +0000

    fixing the return type by initializing the return fields with 0 each. Before: return Record<Transaction["paymentMethod"], number | undefined> Now: return Record<Transaction["paymentMethod"], number>

--- data-manipulation-in-ts/src/utils/statistics/transactionsPerPaymentMethod.ts
+++ data-manipulation-in-ts/src/utils/statistics/transactionsPerPaymentMethod.ts
@@ -1,12 +1,15 @@
 import type Transaction from "../../types/transaction.types.js";
+import { TRANSACTION_PAY_METHODS } from "../../types/transaction.types.js";
 
 export default function transactionsPerPaymentMethod(
   transactions: Transaction[]
 ): Record<Transaction["paymentMethod"], number> {
   let res = {} as Record<Transaction["paymentMethod"], number>;
 
+  TRANSACTION_PAY_METHODS.forEach((pm) => (res[pm] = 0));
+
   for (let t of transactions) {
-    res[t.paymentMethod] = (res[t.paymentMethod] ?? 0) + 1;
+    res[t.paymentMethod]++;
   }
 
   return res;
--- data-manipulation-in-ts/src/utils/statistics/transactionsPerStatus.ts
+++ data-manipulation-in-ts/src/utils/statistics/transactionsPerStatus.ts
@@ -1,10 +1,16 @@
-import type Transaction from "../../types/transaction.types";
+import type Transaction from "../../types/transaction.types.js";
+import { TRANSACTION_STATUSES } from "../../types/transaction.types.js";
 
 export default function transactionsPerStatus(
   transactions: Transaction[]
 ): Record<Transaction["status"], number> {
-  return transactions.reduce((record, currTransaction) => {
-    record[currTransaction.status] = (record[currTransaction.status] ?? 0) + 1;
-    return record;
-  }, {} as Record<Transaction["status"], number>);
+  let res = {} as Record<Transaction["status"], number>;
+
+  TRANSACTION_STATUSES.forEach((pm) => (res[pm] = 0));
+
+  for (let t of transactions) {
+    res[t.status]++;
+  }
+
+  return res;
 }
